a1d7dc66593a64fc83de0fe5490744c95647e249
Enrico Savioli <enrico.savioli@datalogic.com>
2018-01-16 14:27:00 +0100

    Add ConfigFile class to handle configuration properties.

--- tungsten.cpp
+++ tungsten.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
+#include "ConfigFile.h"
+
 int main(int argc, char *argv[])
 {
 	std::cout << "Hello, World!!" << std::endl;
+
+	tungsten::ConfigFile cfg("..\\config\\config.cfg");
+
+	bool exists = cfg.keyExists("prova");
+	std::string prova = cfg.getValueOfKey<std::string>("prova", "ERROR!");
 
 	cv::VideoCapture cap(0);
 	if (!cap.isOpened())
@@ -14,7 +21,9 @@ int main(int argc, char *argv[])
 		cv::Mat frame;
 		cap >> frame;
 
-		cv::imshow("captured", frame);
+		cv::Mat bgr[3];
+		cv::split(frame, bgr);
+		cv::imshow("grayscale", bgr[0]);
 		if (cv::waitKey(30) > 0) break;
 	}
 
