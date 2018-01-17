c90e5b4ef678ce0741392cfd18bbd562aff48af3
Enrico Savioli <enrico.savioli@datalogic.com>
2018-01-17 09:17:53 +0100

    Show the histograms of the color distribution in the input frame.

--- tungsten.cpp
+++ tungsten.cpp
@@ -53,6 +53,54 @@ cv::Mat preprocess_frame(cv::Mat &image, tungsten::ConfigFile cfg)
 	return image;
 }
 
+void create_histogram_image(cv::Mat &input, cv::Mat &output)
+{
+	std::vector<cv::Mat> bgr_planes;
+	cv::split(input, bgr_planes);
+
+	/// Establish the number of bins
+	int histSize = 256;
+
+	/// Set the ranges ( for B,G,R) )
+	float range[] = { 0, 256 };
+	const float* histRange = { range };
+
+	bool uniform = true; bool accumulate = false;
+
+	cv::Mat b_hist, g_hist, r_hist;
+
+	/// Compute the histograms:
+	calcHist(&bgr_planes[0], 1, 0, cv::Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate);
+	calcHist(&bgr_planes[1], 1, 0, cv::Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate);
+	calcHist(&bgr_planes[2], 1, 0, cv::Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate);
+
+	// Draw the histograms for B, G and R
+	int hist_w = 512; int hist_h = 400;
+	int bin_w = cvRound((double)hist_w / histSize);
+
+	cv::Mat histImage(hist_h, hist_w, CV_8UC3, cv::Scalar(0, 0, 0));
+
+	normalize(b_hist, b_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
+	normalize(g_hist, g_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
+	normalize(r_hist, r_hist, 0, histImage.rows, cv::NORM_MINMAX, -1, cv::Mat());
+
+	/// Draw for each channel
+	for (int i = 1; i < histSize; i++)
+	{
+		line(histImage, cv::Point(bin_w*(i - 1), hist_h - cvRound(b_hist.at<float>(i - 1))),
+			cv::Point(bin_w*(i), hist_h - cvRound(b_hist.at<float>(i))),
+			cv::Scalar(255, 0, 0), 2, 8, 0);
+		line(histImage, cv::Point(bin_w*(i - 1), hist_h - cvRound(g_hist.at<float>(i - 1))),
+			cv::Point(bin_w*(i), hist_h - cvRound(g_hist.at<float>(i))),
+			cv::Scalar(0, 255, 0), 2, 8, 0);
+		line(histImage, cv::Point(bin_w*(i - 1), hist_h - cvRound(r_hist.at<float>(i - 1))),
+			cv::Point(bin_w*(i), hist_h - cvRound(r_hist.at<float>(i))),
+			cv::Scalar(0, 0, 255), 2, 8, 0);
+	}
+
+	output = histImage;
+}
+
 int main(int argc, char *argv[])
 {
 	std::cout << "Hello, World!!" << std::endl;
@@ -60,6 +108,7 @@ int main(int argc, char *argv[])
 	tungsten::ConfigFile cfg("..\\config\\config.cfg");
 
 	bool separate_channels = cfg.getValueOfKey("separate_channels");
+	bool show_histograms = cfg.getValueOfKey("show_histograms");
 	std::string image_store_path = cfg.getValueOfKey<std::string>("image_store_path", "");
 
 	cv::VideoCapture cap(0);
@@ -76,6 +125,13 @@ int main(int argc, char *argv[])
 	{
 		cv::Mat frame;
 		cap >> frame;
+
+		if (show_histograms)
+		{
+			cv::Mat histograms;
+			create_histogram_image(frame, histograms);
+			cv::imshow("histograms", histograms);
+		}
 
 		cv::Mat img;
 		img = preprocess_frame(frame, cfg);
